a6282a8ec2f97d7f5d1d9ae2b6c7a72048556e3f
vinicius_cassin <vinicius.cassin@hotmail.com>
2023-05-10 20:35:53 -0300

    Fix build gradle and application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,14 +21,12 @@ extra["springCloudVersion"] = "2022.0.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("io.mockk:mockk:1.13.4")
 
 }
